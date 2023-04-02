8e4e0058531f6806acc2fe1a5b5d29ea2fc2325c
Sami Kalammallah <skalexsong@gmail.com>
2023-04-02 15:09:10 +0700

    feat : implement proceed to whatsapp
    Signed-off-by: Sami Kalammallah <skalexsong@gmail.com>

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -1,18 +1,22 @@
+import { Key, useCallback, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { Button, List, Sheet, Toolbar } from "konsta/react";
 import { useGlobalState } from "@/context/Provider";
 import { toggleBasket } from "@/context/actions";
-import { Block, Button, Link, List, Sheet, Toolbar } from "konsta/react";
-import { MdClose } from "react-icons/md";
 import FoodItem from "./foods/FoodItem";
 import { FoodData } from "@/interfaces/food";
-import { Key, useCallback, useEffect, useState } from "react";
+import { TEMPLATE_WA_LINK } from "@/utils/constant/WHATSAPP";
+import { BiCartAlt } from "react-icons/bi";
 
 const Basket = () => {
+  const router = useRouter();
   const [{ basket, isBasketOpened }, dispatch]: any = useGlobalState();
   const [totalPrice, setTotalPrice] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const getTotalPrice = useCallback(() => {
+  const processBaskets = useCallback(() => {
     const sum = basket
-      .map((item: any) => item.price)
+      .map((item: FoodData) => item.price)
       .reduce(
         (partialSum: number, itemPrice: number) => partialSum + itemPrice,
         0
@@ -21,9 +25,36 @@ const Basket = () => {
     setTotalPrice(sum);
   }, [basket]);
 
+  const handleCheckout = () => {
+    setLoading(true);
+
+    const itemNameAndQuantity = basket.map((item: FoodData) => {
+      const formatTitle = item.title.replace(" ", "%20");
+      return {
+        name: formatTitle,
+        quantity: item.quantity,
+      };
+    });
+
+    let templateFoodAndItem = "";
+
+    itemNameAndQuantity.forEach(
+      ({ name, quantity }: any) =>
+        (templateFoodAndItem += `*${name}*%20x%20*${quantity}*%2C%20`)
+    );
+
+    const finalUrl = TEMPLATE_WA_LINK(templateFoodAndItem, totalPrice);
+
+    setTimeout(() => {
+      setLoading(false);
+      router.push(finalUrl);
+      dispatch(toggleBasket());
+    }, 1000);
+  };
+
   useEffect(() => {
-    getTotalPrice();
-  }, [getTotalPrice]);
+    processBaskets();
+  }, [processBaskets]);
 
   return (
     <Sheet
@@ -32,35 +63,36 @@ const Basket = () => {
       onBackdropClick={() => dispatch(toggleBasket())}
     >
       <Toolbar top>
-        <div className="left" />
-        <div className="right">
-          <Link toolbar onClick={() => dispatch(toggleBasket())}>
-            <MdClose className="text-lg" />
-          </Link>
+        <div className="flex items-center justify-center w-full">
+          <span className="font-bold uppercase">Keranjang Pesanan</span>
         </div>
       </Toolbar>
-      <Block>
-        <List>
-          {basket.map((data: FoodData, index: Key) => (
-            <FoodItem key={index} data={data} />
-          ))}
-        </List>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum ad
-          excepturi nesciunt nobis aliquam. Quibusdam ducimus neque
-          necessitatibus, molestias cupiditate velit nihil alias incidunt,
-          excepturi voluptatem dolore itaque sapiente dolores!
-        </p>
-        Rp. {totalPrice}
-        <div className="mt-4">
-          <Button
-            className="rounded-md"
-            onClick={() => dispatch(toggleBasket())}
-          >
-            Checkout
-          </Button>
+
+      <List>
+        {basket.map((data: FoodData, index: Key) => (
+          <FoodItem key={index} data={data} clickable={false} />
+        ))}
+      </List>
+
+      <div className="flex items-center justify-start w-full border-t dark:border-t-slate-700 p-4">
+        <div className="bg-slate-500 text-white dark:bg-white dark:text-primary rounded-full p-2 mr-2">
+          <BiCartAlt className="text-xl" />
         </div>
-      </Block>
+
+        <div className="whitespace-nowrap flex items-start flex-col">
+          <span>Total Harga</span>
+          <span className="font-bold text-lg text-primary">
+            Rp. {totalPrice}
+          </span>
+        </div>
+
+        <Button
+          className="ml-auto rounded-xl py-6 w-[150px] shadow"
+          onClick={handleCheckout}
+        >
+          {loading ? "Membuka whatsapp" : "Beli Sekarang"}
+        </Button>
+      </div>
     </Sheet>
   );
 };
