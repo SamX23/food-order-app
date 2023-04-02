ac933f135ba20e01bb778b30a2b4ea80400f013d
Sami Kalammallah <skalexsong@gmail.com>
2023-04-02 13:04:32 +0700

    feat : add total price
    Signed-off-by: Sami Kalammallah <skalexsong@gmail.com>

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -4,10 +4,26 @@ import { Block, Button, Link, List, Sheet, Toolbar } from "konsta/react";
 import { MdClose } from "react-icons/md";
 import FoodItem from "./foods/FoodItem";
 import { FoodData } from "@/interfaces/food";
-import { Key } from "react";
+import { Key, useCallback, useEffect, useState } from "react";
 
 const Basket = () => {
   const [{ basket, isBasketOpened }, dispatch]: any = useGlobalState();
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+
+  const getTotalPrice = useCallback(() => {
+    const sum = basket
+      .map((item: any) => item.price)
+      .reduce(
+        (partialSum: number, itemPrice: number) => partialSum + itemPrice,
+        0
+      );
+
+    setTotalPrice(sum);
+  }, [basket]);
+
+  useEffect(() => {
+    getTotalPrice();
+  }, [getTotalPrice]);
 
   return (
     <Sheet
@@ -19,7 +35,7 @@ const Basket = () => {
         <div className="left" />
         <div className="right">
           <Link toolbar onClick={() => dispatch(toggleBasket())}>
-            <MdClose />
+            <MdClose className="text-lg" />
           </Link>
         </div>
       </Toolbar>
@@ -35,8 +51,14 @@ const Basket = () => {
           necessitatibus, molestias cupiditate velit nihil alias incidunt,
           excepturi voluptatem dolore itaque sapiente dolores!
         </p>
+        Rp. {totalPrice}
         <div className="mt-4">
-          <Button onClick={() => dispatch(toggleBasket())}>Checkout</Button>
+          <Button
+            className="rounded-md"
+            onClick={() => dispatch(toggleBasket())}
+          >
+            Checkout
+          </Button>
         </div>
       </Block>
     </Sheet>
