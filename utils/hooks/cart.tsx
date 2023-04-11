4d50a523097bc3edb65215775f72d4855f45cf46
Sami Kalammallah <skalexsong@gmail.com>
2023-04-11 22:10:21 +0700

    update : toast behaviour
    Signed-off-by: Sami Kalammallah <skalexsong@gmail.com>

--- utils/hooks/cart.tsx
+++ utils/hooks/cart.tsx
@@ -1,26 +1,23 @@
 import { useGlobalState } from "@/context/Provider";
-import { FoodData } from "@/interfaces/food";
 import { useCallback, useEffect, useState } from "react";
 
 const useCartListener = () => {
   const [{ basket }]: any = useGlobalState();
   const [toastOpened, setToastOpened] = useState<boolean>(false);
   const [lastItemName, setLastItemName] = useState<string>("");
+  const basketLastItem = basket.at(-1);
 
   const openToast = useCallback((setter: (arg0: boolean) => void) => {
-    // close other toast
     setter(true);
     setTimeout(() => setter(false), 1500);
   }, []);
 
   useEffect(() => {
     if (basket.length > 0) {
-      const latestItem: FoodData = basket.at(-1);
-      console.log("Recently Added : ", latestItem);
-      setLastItemName(latestItem.title);
+      setLastItemName(`${basketLastItem?.title} ditambakan ke keranjang !`);
       openToast(setToastOpened);
     }
-  }, [basket, openToast]);
+  }, [basket.length, basketLastItem?.title, openToast]);
 
   return { lastItemName, toastOpened, setToastOpened };
 };
