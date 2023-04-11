e3b0bb7305c69166d13f6f76b9d0d906448fe8c5
Sami Kalammallah <skalexsong@gmail.com>
2023-04-11 21:58:26 +0700

    update : add more action and reducer
    Signed-off-by: Sami Kalammallah <skalexsong@gmail.com>

--- context/reducer.ts
+++ context/reducer.ts
@@ -3,7 +3,9 @@ import {
   ADD_TO_BASKET,
   ActionTypes,
   INCREASE_ITEM,
+  DECREASE_ITEM,
   TOGGLE_BASKET,
+  REMOVE_FROM_BASKET,
 } from "./actions";
 
 export const initialValue: InitialValue = {
@@ -38,6 +40,36 @@ export const reducer = (
       return {
         ...state,
         isBasketOpened: !state.isBasketOpened,
+      };
+
+    case INCREASE_ITEM:
+      return {
+        ...state,
+        basket: state.basket.map((item) =>
+          item.id === action.data.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        ),
+      };
+
+    case DECREASE_ITEM:
+      if (action.data.quantity === 1) {
+        return { ...state };
+      }
+
+      return {
+        ...state,
+        basket: state.basket.map((item) =>
+          item.id === action.data.id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        ),
+      };
+
+    case REMOVE_FROM_BASKET:
+      return {
+        ...state,
+        basket: state.basket.filter((item) => item.id !== action.data.id),
       };
 
     default:
