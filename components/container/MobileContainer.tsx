1b71f099ab1beedb27c7f1adafb3e6868a915bdf
Sami Kalammallah <skalexsong@gmail.com>
2023-04-21 06:50:27 +0700

    feat : implement dark mode
    Signed-off-by: Sami Kalammallah <skalexsong@gmail.com>

--- components/container/MobileContainer.tsx
+++ components/container/MobileContainer.tsx
@@ -1,6 +1,24 @@
+import { useEffect } from "react";
+import { useGlobalState } from "@/context/Provider";
 import { App as KonstaApp } from "konsta/react";
+import { toggleDarkMode } from "@/context/actions";
 
 const MobileContainer = ({ children }: any) => {
+  const [_, dispatch]: any = useGlobalState();
+
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      dispatch(toggleDarkMode());
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [dispatch]);
+
   return (
     <KonstaApp theme="ios" className="max-w-md mx-auto" safeAreas>
       {children}
