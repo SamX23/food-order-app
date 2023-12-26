3528c50c33a0661d78a002cbe7e2333eb396aa66
Sami Kalammallah <skalexsong@gmail.com>
2023-12-26 23:48:20 +0700

    chore: add dashboard file

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,62 @@
+import supabase from "@/utils/supabase";
+import {
+  Suspense,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+
+interface StateProps {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+}
+
+const DashboardPage = () => {
+  const [data, setData] = useState<number>(100);
+  const getData = useCallback(async () => {}, []);
+
+  useEffect(() => {
+    console.log({ data });
+    getData();
+  }, [data, getData]);
+
+  const Total = useMemo(() => data * 100, []);
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const onSubmit = async () => {
+    console.log({ form: formRef });
+    formRef.current?.submit();
+  };
+
+  return (
+    <div>
+      DashboardPage
+      <hr />
+      <h1>{data}</h1>
+      <h2>{Total}</h2>
+      <hr />
+      <button
+        //   onClick={() => setData((prev) => prev + 1)}
+        onClick={onSubmit}
+      >
+        ADD
+      </button>
+      <form ref={formRef}>
+        <input type="date" name="dates" id="dates" />
+      </form>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div>TOTAL USER</div>
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div>TOTAL TRANSACTION</div>
+      </Suspense>
+    </div>
+  );
+};
+
+export default DashboardPage;
